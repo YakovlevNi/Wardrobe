a15b0fadabaae35942cde3662a2c0a31a21340eb
Yakovlev Nikita <yakovlev_ni@mail.ru>
2023-01-31 01:38:54 +0300

    1

---
+++ .idea/src/test/java/com/example/Wardrobe/WardrobeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Wardrobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WardrobeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
